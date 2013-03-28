a8915b7056cc1dba7bc127d1c051727a512b8acf
Jano Sousa <sollykma@gmail.com>
2013-03-27 23:10:14 -0300

    reenviando os arquivos

---
+++ admin/language/portuguese-br/trabalheconosco/setting.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']            = 'Configurações';
+
+// Text
+$_['text_curriculum']          = 'Currículo';
+$_['text_success']             = 'Configurações modificadas com sucesso!';
+
+
+//entry
+$_['entry_file_extension_allowed_curriculum']   = 'Extensões Permitidas para Upload:<br /><span class="help">Adicionar quais extensões de arquivos são permitidos que os usuários enviem junto  o currículo. Use valores separados por vírgulas.</span>';
+$_['entry_file_mime_allowed_curriculum']      = 'Allowed File Mime Types:<br /><span class="help">Add which file mime types are allowed to be uploaded. Use a new line for each value.</span>';
+$_['entry_maintenance_curriculum']            = 'Ativar Modo de Manutenção:<br /><span class="help">Ativando essa opção, a página não estará mais acessivel ao publico!</span>';
+$_['entry_captcha_curriculum']                 = 'Ativar captcha:<br /><span class="help">Com essa opção ativada vai ajudar a bloquear spam no recebimento de currículo!</span>';
+// Error
+$_['error_permission']         = 'Atenção: Você não possui permissão para modificar as configurações!';
+
+?>
